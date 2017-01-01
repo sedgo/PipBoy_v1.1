85a678945b61e478cc408b64e2504be642dd28ea
SedG Warmaster <sedg.warmaster@hotmail.com>
2017-01-02 02:24:15 +0900

    Video test!

--- app/src/main/java/com/example/sedgw/pipboy_v11/MainActivity.java
+++ app/src/main/java/com/example/sedgw/pipboy_v11/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.sedgw.pipboy_v11;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         //FullScreen options
-        //requestWindowFeature(Window.FEATURE_NO_TITLE); //NE DJOBAET PADLA!
-        //getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
+
+        //path for video
+        String videoSource ="http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_1mb.mp4";
+
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
+        videoView.setVideoURI(Uri.parse(videoSource));
+
+        videoView.setMediaController(new MediaController(this));
+        videoView.requestFocus(0);
+        videoView.start();
     }
 }
