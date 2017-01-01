5372a70d48130e1127e9b0265e266635091f606d
SedG Warmaster <sedg.warmaster@hotmail.com>
2017-01-02 02:09:00 +0900

    Add theme for activity without action bar

---
+++ app/src/main/java/com/example/sedgw/pipboy_v11/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.sedgw.pipboy_v11;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //FullScreen options
+        //requestWindowFeature(Window.FEATURE_NO_TITLE); //NE DJOBAET PADLA!
+        //getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+    }
+}
